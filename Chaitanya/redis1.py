2c61275d4371515d74eeb6fa923564ae0fd9e643
NairChaitanya <32630326+NairChaitanya@users.noreply.github.com>
2018-01-05 10:11:44 +0530

    Update redis1.py

--- Chaitanya/redis1.py
+++ Chaitanya/redis1.py
@@ -5,8 +5,7 @@ import os
 class CustomCollector(object):
 	def collect(self):
 		print 'Go to localhost'
-		matrices={'total_connections_received':'Total connected clients',
-'connected_slaves':'Total connected slaves',
+		matrices={'connected_slaves':'Total connected slaves',
 'instantaneous_ops_per_sec':'Number of commands processed per second',
 'keyspace_misses':'Number of failed lookup of keys',
 'used_memory':'Amount Of memory used by redis',
@@ -15,7 +14,9 @@ class CustomCollector(object):
 'blocked_clients':'Clients blocked while waiting',
 'rdb_last_save_time':'Unix timestamp to last save to disk',
 'rdb_changes_since_last_save':'Number of changes to database since last dump',
-'rejected_connections':'Number of connections rejected due to hitting maxclient limit'}
+'rejected_connections':'Number of connections rejected due to hitting maxclient limit',
+'evicted_keys':'Number Of keys removed due to reaching maxmemory limit',
+'keyspace_hits':'Number Of keys in database'}
 		r=redis.StrictRedis()
 		user1=r.info()
 		for name,value in user1.iteritems():
@@ -24,6 +25,17 @@ class CustomCollector(object):
 				metric = Metric(name,matrices[name],'summary')
 				metric.add_sample(name,value=value1,labels={})
 				yield metric
+			if name=='keyspace_hits':
+				kh=value
+			if name=='keyspace_misses':
+				km=value
+		try:
+			hitrate=kh/(kh+km)
+		except ZeroDivisionError:
+			hitrate=0
+		metric=Metric('hit_rate','keyspace_hits/(keyspace_hits+keyspace_misses)','summary')
+		metric.add_sample('hit_rate',value=hitrate,labels={})
+		yield metric
 		
 if __name__ == '__main__':
 	#pass port number as argument
