a94bbf1fc09185578a2633ad2133409b47ae84e2
GaykarSanket <sanketgaykar.333@gmail.com>
2018-02-07 22:27:13 +0530

    multiple arguments added

--- Sanket/service_exporter.py
+++ Sanket/service_exporter.py
@@ -4,17 +4,18 @@ import sys, os
 import argparse
 
 SERVICE = 'all'
-EXPORTER_PORT = 9012
+EXPORTER_PORT = 9019
 
 class SuperVisor(object):
     def __init__(self, endpoint):
         self._endpoint = endpoint
 
     def collect(self):
-        print 'Go to localhost:9012'
+        print 'Go to localhost:9019'
         SERVICE=self._endpoint
 
 ############################## supervisor part ############################
+        
         n = int(os.popen('sudo supervisorctl status all | wc -l').readlines()[0])
 
         metric = Metric('supervisord_total_services', 'Total number of services in supervisor','summary')
@@ -50,34 +51,35 @@ class SuperVisor(object):
 ################################## system services part #############################
 
         if SERVICE!='all':
-            loaded_status = (os.popen('service '+SERVICE+' status').readlines()[1]).strip()[8:14]
-            #print loaded_status
-            if loaded_status=='loaded':
-                lflag=1
-            else:
-                lflag=0
- 
-            metric = Metric(SERVICE+'_exists_status', 'Service exists(1) or not(0)','summary')
-            metric.add_sample(SERVICE+'_exists_status', value=lflag, labels={})
-            yield metric
+            l=len(SERVICE)
+            for i in range(0,l):
+                loaded_status = (os.popen('service '+SERVICE[i]+' status').readlines()[1]).strip()[8:14]
+                #print loaded_status
+                if loaded_status=='loaded':
+                    lflag=1
+                else:
+                    lflag=0 
+                metric = Metric(SERVICE[i]+'_exists_status', 'Service exists(1) or not(0)','summary')
+                metric.add_sample(SERVICE[i]+'_exists_status', value=lflag, labels={})
+                yield metric
 
-            active_status = (os.popen('service '+SERVICE+' status').readlines()[2]).strip()[8:24]
-            #print active_status
-            if active_status=='active (running)':
-                rflag=1
-            else:
-                rflag=0
-
-            metric = Metric(SERVICE+'_running_status', 'Service running(1) or dead(0)','summary')
-            metric.add_sample(SERVICE+'_running_status', value=rflag, labels={})
-            yield metric
+                active_status = (os.popen('service '+SERVICE[i]+' status').readlines()[2]).strip()[8:24]
+                #print active_status
+                if active_status=='active (running)':
+                    rflag=1
+                else:
+                    rflag=0
+                metric = Metric(SERVICE[i]+'_running_status', 'Service running(1) or dead(0)','summary')
+                metric.add_sample(SERVICE[i]+'_running_status', value=rflag, labels={})
+                yield metric
 
 if __name__ == '__main__':
     # main
     parser = argparse.ArgumentParser(description='arguments')
-    parser.add_argument('service', nargs='?', const=1, type=str, default=SERVICE)
+    parser.add_argument('service', nargs='*', type=str, default=SERVICE)
     args = parser.parse_args()
     SERVICE=args.service
+
     try:
         start_http_server(EXPORTER_PORT)
     except:
