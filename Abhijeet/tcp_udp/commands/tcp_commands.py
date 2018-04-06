e0308e9ecf4ea0b04d4a9c930483e18d4b725881
Ubuntu <ubuntu@Bexample.com>
2018-04-06 07:59:30 +0000

    final

--- Abhijeet/tcp_udp/commands/tcp_commands.py
+++ Abhijeet/tcp_udp/commands/tcp_commands.py
@@ -5,56 +5,56 @@ tcp_udp_metrics=[
 		'name' : 'sockstat_sockets',
 		'command' : 'cat /proc/net/sockstat| awk "/sockets/ { print $3 }" | cut -d":" -f2 | cut -d " " -f3',
 		'description' : 'total number of sockets used',
-		'type' : 'summary'
+		'type' : 'gauge'
 	},
 
 	{
 		'name' : 'sockstat_tcp_inuse',
 		'command' : "cat /proc/net/sockstat| awk '/TCP/ { print $3 }'",
 		'description' : 'total established connections',
-		'type' : 'summary'
+		'type' : 'gauge'
 	},
 
 	{
 		'name' : 'sockstat_tcp_orphan',
 		'command' : "cat /proc/net/sockstat| awk '/TCP/ { print $3 }'",
 		'description' : 'Orphaned tcp connections(not attached to any user file handle)',
-		'type' : 'summary'
+		'type' : 'gauge'
 	},
 
 	{
 		'name' : 'sockstat_tcp_timewait',
 		'command' : "cat /proc/net/sockstat| awk '/TCP/ { print $7 }'",
 		'description' : 'TIME_WAIT connections(millisec)',
-		'type' : 'summary'
+		'type' : 'gauge'
 	},
 
 	{
 		'name' : 'sockstat_tcp_allocated',
 		'command' : "cat /proc/net/sockstat| awk '/TCP/ { print $9 }'",
 		'description' : 'TCP sockets allocated( All type for example, ESTABLISH, CLOSE_WAIT, TIME_WAIT, etc)',
-		'type' : 'summary'
+		'type' : 'gauge'
 	},
 
 	{
 		'name' : 'sockstat_tcp_mem',
 		'command' : "cat /proc/net/sockstat| awk '/TCP/ { print $11 }'",
 		'description' : 'total memory for TCP socket(KiloBytes)',
-		'type' : 'summary'
+		'type' : 'gauge'
 	},
 
 	{
 		'name' : 'sockstat_udp_inuse',
 		'command' : "cat /proc/net/sockstat| awk '/UDP:/ { print $3 }'",
 		'description' : 'total established connections',
-		'type' : 'summary'
+		'type' : 'gauge'
 	},
 
 	{
 		'name' : 'sockstat_udp_mem',
 		'command' : "cat /proc/net/sockstat| awk '/UDP:/ { print $5 }'",
 		'description' : 'total memory for udp socket(KB)',
-		'type' : 'summary'
+		'type' : 'gauge'
 	},
 
 
