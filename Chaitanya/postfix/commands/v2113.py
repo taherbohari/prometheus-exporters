60ff257aff6d01662ea10f53f566e3655e8f4659
Ubuntu <ubuntu@Bexample.com>
2018-04-06 07:55:42 +0000

    final

--- Chaitanya/postfix/commands/v2113.py
+++ Chaitanya/postfix/commands/v2113.py
@@ -4,35 +4,35 @@ postfix_metrices=[
     'name': 'total_mails',
     'command': "pqshell --summary |awk '/Total mails in queue:/ {print $5}'",
     'desc': 'Number of mails in the queue',
-    'type': 'summary',
+    'type': 'gauge',
     'data_type': 'integer',
     },
     {
     'name': 'status_active',
     'command': "pqshell --summary |awk '/Active/ {print $2}'",
     'desc': 'Number of mails with active status',
-    'type': 'summary',
+    'type': 'gauge',
     'data_type': 'integer'
     },
     {
     'name': 'status_deferred',
     'command': "pqshell --summary |awk '/Deferred/ {print $2}'",
     'desc': 'Number of mails with deferred status',
-    'type': 'summary',
+    'type': 'gauge',
     'data_type': 'integer'
     },
     {
     'name': 'status_hold',
     'command': "pqshell --summary |awk '/Hold/ {print $2}'",
     'desc': 'Number of mails with hold status',
-    'type': 'summary',
+    'type': 'gauge',
     'data_type': 'integer'
     },
     {
     'name': 'queue_size',
     'command': "pqshell --summary |awk '/Total queue size/ {print $4}'",
     'desc': 'Size of the mail queue in MB',
-    'type': 'summary',
+    'type': 'gauge',
     'data_type': 'float'
     }
 
